7df5b7706e473a51474a8f53f188d30d731f4ce5
Vincent <thisoneis4business@gmail.com>
2022-06-26 17:20:07 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PioneerRedisPubSub
+
+A description of this package.
---
+++ Sources/PioneerRedisPubSub/PioneerRedisPubSub.swift
@@ -0,0 +1,6 @@
+public struct PioneerRedisPubSub {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
