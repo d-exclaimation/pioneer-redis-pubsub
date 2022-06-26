e514f9489891e99af420027d2f3183f005629dc4
Vincent <thisoneis4business@gmail.com>
2022-06-26 18:40:57 +0700

    Feat: Added first implementation of RedisPubSub

--- Sources/PioneerRedisPubSub/PioneerRedisPubSub.swift
+++ Sources/PioneerRedisPubSub/PioneerRedisPubSub.swift
@@ -1,6 +1,70 @@
-public struct PioneerRedisPubSub {
-    public private(set) var text = "Hello, World!"
+//
+//  PioneerRedisPubSub.swift
+//  PioneerRedisPubSub
+//
+//  Created by d-exclaimation on 17:56.
+//
 
-    public init() {
+import Foundation
+import NIOFoundationCompat
+import class Pioneer.Broadcast
+import protocol RediStack.RedisClient
+
+/// RedisPubSub is a Redis channel backed pubsub structure for managing AsyncStreams in a concurrent safe way utilizing Actors.
+public struct RedisPubSub: Sendable {
+    /// An actor for dispatching Redis channel subscription to multiple broadcasting
+    actor Dispatcher {
+        /// RedisClient used to create a channel subscription
+        private let redis: RedisClient
+
+        /// All broadcasting for all channels
+        private var broadcasting: [String: Broadcast<Data>] = [:]
+
+        init(_ redis: RedisClient) {
+            self.redis = redis
+        }
+
+        /// Get the AsyncStream for the specific Redis channel
+        /// - Parameter channel: The Redis channel name
+        /// - Returns: An AsyncStream **just** for the subscriber
+        internal func downstream(to channel: String) async -> AsyncStream<Data> {
+            let broadcast = await subscribe(to: channel)
+            let downstream = await broadcast.downstream()
+            return downstream.stream
+        }
+
+        /// Get the appropriate broadcasting for the Redis channel, if hasn't exist yet, create a new one
+        /// - Parameter channel: The Redis channel name
+        /// - Returns: A broadcast actor for the channel
+        private func subscribe(to channel: String) async -> Broadcast<Data> {
+            if let broadcast = broadcasting[channel] {
+                return broadcast
+            }
+            let broadcast = await redis.broadcast(for: .init(channel))
+            broadcasting[channel] = broadcast
+            return broadcast
+        }
+
+        /// Publish a data to a certain Redis channel
+        /// - Parameters:
+        ///   - channel: The Redis channel name
+        ///   - value: The data to be published
+        internal func publish(for channel: String, _ value: Data) async {
+            let _ = try? await redis.publish(value, to: .init(channel)).get()
+        }
+
+        /// Close Redis subscription and unsubscribed all subscriber for the channel
+        /// - Parameter channel: The Redis channel name
+        internal func close(for channel: String) async {
+            try? await redis.unsubscribe(from: .init(channel)).get()
+            await broadcasting[channel]?.close()
+        }
+    }
+
+    /// The internal dispatcher for Dispatcher
+    internal let dispatcher: Dispatcher
+
+    public init(_ redis: RedisClient) {
+        self.dispatcher = .init(redis)
     }
 }
