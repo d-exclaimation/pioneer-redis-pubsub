c3558dedc49e1a9190b9156483a11ad5d03389bd
Vincent <thisoneis4business@gmail.com>
2023-10-22 16:43:12 +1300

    fix: fixed issue with package being moved

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/d-exclaimation/pioneer", from: "1.0.0"),
-        .package(url: "https://gitlab.com/mordil/RediStack.git", from: "2.0.0-gamma.1"),
+        .package(url: "https://github.com/swift-server/RediStack.git", from: "1.4.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/PioneerRedisPubSub/Extensions/RedisClient+Broadcast.swift
+++ Sources/PioneerRedisPubSub/Extensions/RedisClient+Broadcast.swift
@@ -10,7 +10,26 @@ import class Pioneer.Broadcast
 import protocol RediStack.RedisClient
 import enum RediStack.RESPValue
 import struct RediStack.RedisChannelName
-import typealias RediStack.RedisPubSubEventReceiver
+
+///
+/// This code needed to be updated when RediStack reaches v2.0.0
+/// ```swift
+/// try await subscribe(to: channel) { event in
+///     switch (event) {
+///         case .message(publisher:_, message: .bulkString(.some(let buffer))):
+///             let data = Data(buffer: buffer)
+///             Task {
+///                 await broadcast.publish(data)
+///             }  
+///         case .unsubscribed(key: _, currentSubscriptionCount: _, source: _):
+///             Task {
+///                 await broadcast.close()
+///             }
+///         default:
+///             break;
+///     }
+/// }.get()
+/// ```
 
 extension RedisClient {
     /// Subscribes the client to the specified Redis channels and publish the messages into a broadcast
@@ -19,22 +38,22 @@ extension RedisClient {
     @discardableResult
     public func broadcast(given broadcast: Broadcast<Data> = .init(), for channel: RedisChannelName) async -> Broadcast<Data> {
         do {
-            try await subscribe(to: channel) { event in
-                switch (event) {
-                    case .message(publisher:_, message: .bulkString(.some(let buffer))):
-                        let data = Data(buffer: buffer)
-                        Task {
-                            await broadcast.publish(data)
-                        }  
-                    case .unsubscribed(key: _, currentSubscriptionCount: _, source: _):
-                        Task {
-                            await broadcast.close()
-                        }
-                    default:
-                        break;
+            let future = subscribe(
+                to: channel,
+                messageReceiver: { _, msg in
+                    guard case .bulkString(.some(let buffer)) = msg else { return }
+                    let data = Data(buffer: buffer)
+                    Task {
+                        await broadcast.publish(data)
+                    } 
+                }, 
+                onUnsubscribe: { _, _ in 
+                    Task {
+                        await broadcast.close()
+                    }
                 }
-
-            }.get()
+            )
+            try await future.get()
         } catch {
             await broadcast.close()
         }
